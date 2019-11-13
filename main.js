d8452ca6bd8507c406e93319a5fc1fd339cd9aa7
Pasquale Paudice <pasquale.paudice10@gmail.com>
2019-11-13 13:04:05 +0100

    html+css+js

---
+++ main.js
@@ -0,0 +1,6 @@
+var nome = prompt("qual è il tuo nome?");
+var cognome = prompt("qual è il tuo cognome?");
+var colore_preferito = prompt("qual è il tuo colore preferito?");
+
+console.log(nome + cognome + colore_preferito);
+document.getElementById('Password').innerHTML= nome + cognome + colore_preferito ;
